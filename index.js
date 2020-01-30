30a3fda39fb8a88b2478e8a1468fedd0db56a785
zaheeruddinfaiz <zaheerfazy@gmail.com>
2020-01-30 23:02:51 +0500

    Second Commit

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ ls.stderr.on('data', (data) => {
   console.error(`stderr: ${data}`);
 });
 
-ls.on('close', (code) => {
-  console.log(`child process exited with code ${code}`);
-});
+// ls.on('close', (code) => {
+//   console.log(`child process exited with code ${code}`);
+// });
